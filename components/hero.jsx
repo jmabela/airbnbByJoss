06f98e104f856790d6d0bbfd6bb7c51b447caa7a
jmabela <josselyn53@gmail.com>
2023-09-27 13:03:01 -0700

    added hero section and styling

---
+++ components/hero.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero(){
+    return (
+        <section className="hero">
+            <img src="../images/Group77.png" className="hero--image"></img>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+        
+    )
+}
---
+++ components/navBar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NavBar(){
+    return (
+        <nav>
+            <img src="../images/airbnb 1.png" className="nav--logo"></img>
+        </nav>
+        
+    )
+}
