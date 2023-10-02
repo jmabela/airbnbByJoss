c114a0a9c1b9c497a7b1ecc5a2e372d105c2ec41
jmabela <josselyn53@gmail.com>
2023-10-02 14:25:05 -0700

    added some css

--- App.jsx
+++ App.jsx
@@ -2,22 +2,31 @@ import React from "react"
 import NavBar from "./components/navBar"
 import Hero from "./components/hero"
 import Card from "./components/card"
+import Data from "./data"
 
+console.log(Data)
+
+const cardData = Data.map(item => {
+    return (
+        <Card
+        img = {item.coverImg}
+        star= {"../images/Star.png"}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location= {item.location}
+        title= {item.title}
+        price= {item.price} 
+        />
+
+    )
+});
 
 export default function App() {
     return (
         <div>
             <NavBar />
             <Hero />
-            <Card 
-            img = "../images/image12.png"
-            star= "../images/Star.png"
-            rating ="5.0"
-            reviewCount = {6}
-            country= "USA"
-            title="Life Lessons with Katie Zaferes"
-            price= {136}
-            />
+            {cardData}
         </div>
     )
 }
