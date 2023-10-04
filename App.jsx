02e59ff714fc37d53f3b09a286b9960bf7ad28d4
jmabela <josselyn53@gmail.com>
2023-10-04 08:33:07 -0700

    props

--- App.jsx
+++ App.jsx
@@ -4,18 +4,12 @@ import Hero from "./components/hero"
 import Card from "./components/card"
 import Data from "./data"
 
-console.log(Data)
 
 const cardData = Data.map(item => {
     return (
         <Card
-        img = {item.coverImg}
-        star= {"../images/Star.png"}
-        rating = {item.stats.rating}
-        reviewCount = {item.stats.reviewCount}
-        location= {item.location}
-        title= {item.title}
-        price= {item.price} 
+        key={item.id}
+        item={item}
         />
 
     )
@@ -26,7 +20,10 @@ export default function App() {
         <div>
             <NavBar />
             <Hero />
-            {cardData}
+            <section className="cards-list">
+                {cardData}
+            </section>
+            
         </div>
     )
 }
--- components/card.jsx
+++ components/card.jsx
@@ -1,17 +1,27 @@
 import React from "react"
 
 export default function Card(props) {
+    let badgeText
+
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "Online"
+    }
+
     return (
+        
         <div className="card">
-            <img src={props.img} className="card--photo"></img>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../public/images/${props.item.coverImg}`} className="card--image"></img>
             <div className="class--stats">
-                <img src={props.star} className="card--star"></img>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <img src="../images/Star.png"className="card--star"></img>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
             <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
             
         
